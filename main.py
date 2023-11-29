5ebc4036f38836a5a9bc36b64082819bc5ccf84c
Lucas Zampieri <lzampier@redhat.com>
2023-11-29 18:21:31 -0300

    Fail nicely when the config is wrong
    Signed-off-by: Lucas Zampieri <lzampier@redhat.com>

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def main():
     # Load configuration
     config = read_config(os.path.expanduser('~/.config/extractarr/config.yml'))
 
+    if config is None:
+        print("Failed to read the configuration. Please check if the config file exists and is valid.")
+        return
+
     # Extract relevant settings
     watch_directories = config.get('watch_directories', [])
     check_interval = config.get('check_interval', 5)
+
+    if not watch_directories:
+        print("No directories are configured for watching. Please update the config.yml file.")
+        return
 
     # Check if any directories are configured for watching
     if not watch_directories:
